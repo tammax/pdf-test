30af8a1b2108fad47c12fcb7572da41fb8d8b47f
Takuya Mizukoshi <tam@TakuyanoMacBook-ea.local>
2019-10-29 21:58:34 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    {{ currentPage }} / {{ pageCount }}
+    <pdf
+      :src="src"
+      @num-pages="pageCount = $event"
+      @page-loaded="currentPage = $event"
+      :page="currentPage"
+    ></pdf>
+    <button @click="nextPage">next</button>
+    <button @click="prevPage">prev</button>
+    <ul>
+      <input type="text" v-model="comment" />
+      <button @click="addComment">投稿</button>
+      <li v-for="(comment, index) in comments" :key="index">
+        <div>{{ comment.name }}:{{ comment.text }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import pdf from "vue-pdf";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    pdf
+  },
+  data() {
+    return {
+      currentPage: 1,
+      pageCount: 0,
+      src:
+        "https://firebasestorage.googleapis.com/v0/b/pdf-test-7a1fb.appspot.com/o/%20sample2.pdf?alt=media&token=e232e689-b76b-4beb-9cec-e608d7172c0b",
+      comment: "",
+      comments: []
+    };
+  },
+  methods: {
+    nextPage() {
+      this.currentPage++;
+      this.page++;
+    },
+    prevPage() {
+      this.currentPage--;
+      this.page--;
+    },
+    addComment() {
+      if (this.comment !== "") {
+        let addComment = {
+          name: "匿名",
+          text: `投稿したページ数:${this.currentPage}ページ:${this.comment}`,
+          createdAt: ""
+        };
+        this.comments.push(addComment);
+        this.comment = "";
+      }
+    }
+  },
+  mounted() {
+    let addComment = {
+      name: "匿名",
+      text: `投稿したページ数:${this.currentPage}: 最初のコメント`,
+      createdAt: ""
+    };
+    this.comments.push(addComment);
   }
 };
 </script>
